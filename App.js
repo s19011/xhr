c4e6787b5a8475ea03c9328ced58e38a0f22397f
tenma <s19011@std.it-college.ac.jp>
2020-01-15 15:56:51 +0900

    0115

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
 import React from 'react';
+import '/App.css'
 
-const Greeting = props => (
-    <li>{props}</li>
-)
-class App extends React.Component() {
-    constructor (props) {
-        super(props) 
-        this.state = {data: [] }
-    }
-    componentDidMount () {
-        fetch('./messages.json')
-            .then(res => res.json())
-            .then(json => json.messages)
-            .then(json => this.state)
-    render () {
-        const greeting = this.state.date
-
-        v => <Greeting type => {v} />
-      
-      <Greeting type={this.state.date[]}/>
-      
-      return (
-          <ul>
-         
-      ) 
-    }
-}
 
  
 export default App;
