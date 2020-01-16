427ad0b9b934940c1d9c88d820e53f30cb795ed6
tenma <s19011@std.it-college.ac.jp>
2020-01-16 14:14:47 +0900

    0116

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TestView = props => (
+  <div>{props.value}</div>
+)
+
+class TestForm extends React.component {
+  constructor (props) {
+      super(props)
+      this.state = {value:''}
+  }
+  handlChange (event) {
+      this.setState({ value: event.target.value})
+  }
+  render () {
+    return (
+        <form action=''>
+            <input type='text' />
+            <input type='submit' />
+            </form>
+    )
+  }
+}
+export class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
 }
 
-export default App;
+export default App
