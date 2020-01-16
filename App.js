9738c92d1b0faebdaa8ea334918be0a5b923bbcd
tenma <s19011@std.it-college.ac.jp>
2020-01-16 13:59:28 +0900

    0116

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import '/App.css'
+import React from 'react'
 
+const TestView = props => (
+  <div>{props.value}</div>
+)
 
- 
-export default App;
+class TestForm extends React.component {
+  constructor (props) {
+      super(props)
+      this.state = {value:''}
+  }
+  handlChange (event) {
+      this.setState({ value: event.target.value})
+  }
+  render () {
+    return (
+        <form action=''>
+            <input type='text' />
+            <input type='submit' />
+            </form>
+    )
+  }
+}
+export class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+}
+
+export default App
